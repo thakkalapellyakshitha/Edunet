b8f65c4c5d6d26a11f90b57e4f2123fa35962dac
thakkalapellyakshitha <thakkalapellyakshitha2004@gmail.com>
2025-09-16 11:03:23 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+import java.util.*;
+class first
+{
+public static void main(String args[]){
+String name;
+Scanner sc=new Scanner(System.in);
+System.out.println("may i know your name");
+name=sc.nextLine();
+System.out.println("welcome"+name);
+}
+}
+
